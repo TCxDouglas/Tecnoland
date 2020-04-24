1d6232cb16148bde5d4dc13e5e977b54ebb42072
TCxDouglas <douglasalexander683@gmail.com>
2020-04-24 01:25:00 -0600

    Buscador y Escoger Temas

--- public/JS/vista-sala/crear-sala.js
+++ public/JS/vista-sala/crear-sala.js
@@ -1,18 +1,30 @@
+let temasEscogidos=[];
 let sala= new Vue({
     el: '#vista-sala',
     data:{
-        temas:[]
+        temas:[],
+        valor:''
     },
     methods:{
-        crearSala : function(){
-            let valor="";
-            fetch(`../../private/PHP/Temas/temas.php?proceso=buscarTemas&valor=${valor}`).then(resp => resp.json()).then(resp => {
+        obtenerTemas : function(){
+            
+            fetch(`../../private/PHP/Temas/temas.php?proceso=buscarTemas&valor=${this.valor}`).then(resp => resp.json()).then(resp => {
                 console.log(resp);
                 this.temas=resp;
             });
+        },
+        escogiendoTemas : function(tema){
+
+            if(!(temasEscogidos.includes(tema)))
+            temasEscogidos.push(tema);
+            console.log(temasEscogidos);
+        },
+        eliminandoTemas : function(tema){
+            temasEscogidos.splice(tema.id, 1);
+            console.log(temasEscogidos);
         }
     },
     created: function () {
-        this.crearSala();
+        this.obtenerTemas();
     }
 });

92f8130bccd19d765c7b99672b547ef32a5363b5
JosueAparicio <josue9aparicio@gmail.com>
2020-05-21 22:58:09 -0600

    chat funcionando

--- public/JS/chat/chat.js
+++ public/JS/chat/chat.js
@@ -1,5 +1,5 @@
 
-const contenido = document.querySelector('#contenidoChat');
+
 var socket = io.connect("http://localhost:3001",{'forceNew':true}),
 mensaje = document.querySelector('#inputMsg'),
 revMesg = [],   
@@ -17,42 +17,60 @@ revMesg = [],
             enviarMensaje(){
               var  envMesg  =
                 {
-                    user: sessionStorage.getItem('uid'),
+                    user: sessionStorage.getItem('displayName'),
                     msg:  document.querySelector('#inputMsg').value.trim(),
-                    fecha: Date.now()
+                    fecha: Date.now(),
+                    uid: sessionStorage.getItem('uid')
                 };
                 socket.emit('enviarMensaje', envMesg);
+                socket.emit('historial');
                 console.log(envMesg);
                 envMesg.user = '';
                 envMesg.msg = '';
                 envMesg.fecha = '';
+                envMesg.uid = '';
+
+                document.querySelector('#inputMsg').value = ''; 
             },
 
         },
         created(){
-            socket.emit('chatHistory');
+            socket.emit('historial');
         }
     });
-    socket.on('recibirMensaje',msg=>{
+   socket.on('recibirMensaje',msg=>{
         revMesg = msg;
-        document.querySelector('#msgg').innerHTML =''+ msg.msg;
         console.log(msg);
     });
-    socket.on('chatHistory',msgs=>{
-         revMesg = [];
-        msgs.forEach(item => {
-            if (sessionStorage.getItem('uid')==item.user){
-                contenido.innerHTML += `
-                <div class="d-flex justify-content-end">
-                <span class="badge badge-pill badge-light" style="margin-top: 5px;">${item.msg}</span>
-            </div>`
+
+    
+        socket.on('historial',msgs=>{
+            
+            var contenido = document.querySelector('#contenidoChat');
+            contenido.innerHTML='';
+             revMesg = [];
+             console.log(msgs);
+             console.log('trae')
+             console.log(contenido);
+            msgs.forEach(item => {
                 
-            } else{
-                contenido.innerHTML += `
+                if (sessionStorage.getItem('uid')===item.uid){
+                   contenido.innerHTML += `
+
+               <div class="d-flex justify-content-end" style="margin-top: 5px;">
+                   <span class="badge badge-pill badge-info">${item.msg}</span>
+               </div>`
+                    
+                } else{
+                    contenido.innerHTML += `
+                    <div class="d-flex justify-content-start">
+                    <small class="text-muted"style="margin-top: 5px;">${item.user}:</small>
+                </div>
                 <div class="d-flex justify-content-start">
-                <span class="badge badge-pill badge-secondary" id="msgg" style="margin-top: 5px;">${item.msg}</span>
-            </div>`
-            }
-             console.log(item.msg);
+                    <span class="badge badge-pill badge-secondary">${item.msg}</span>
+                </div>`
+                }
+                contenido.scrollTop = contenido.scrollHeight;
+            });
         });
-    });
+    

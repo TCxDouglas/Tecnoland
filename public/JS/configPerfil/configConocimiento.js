b436effd7cbcaf2b574e48b66f5b48a00263330f
TCxDouglas <douglasalexander683@gmail.com>
2020-06-20 22:49:00 -0600

    Documentacion de Configuracion de perfil Terminada

--- public/JS/configPerfil/configConocimiento.js
+++ public/JS/configPerfil/configConocimiento.js
@@ -1,3 +1,10 @@
+/**@author Douglas Alexander Hernandez Flores */
+
+/**@function Basico, Intermedio y Avanzado 
+ * Son funciones que asignan el nivel de conocimiento que el usuario cree que posse
+ * Esta funcionalidad esta planeada para una Funcion mas grande a futuro
+ * Que es la de recomendar temas de la plataforma en base a su conocimiento previo
+*/
 var conocimientoEstudiante = new Vue({
     el: '#contConocimiento',
     data: {
